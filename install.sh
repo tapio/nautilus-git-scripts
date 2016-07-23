870cedac3901adf14640e4bbcf5618083517fa67
Christian González <christian.gonzalez@nerdocs.at>
2016-07-23 20:52:12 +0200

    be more verbose
    distinguish between update and install.

--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@
 mkdir -p $HOME/.local/share/nautilus/scripts
 
 if [ -d "$HOME/.local/share/nautilus/scripts" ]; then
+  if [ -d "$HOME/.local/share/nautilus/scripts/Git" ]; then
+    echo "Git script exists as nautilus plugin. Updating..."
+  else
+    echo "Copying Git into Nautilus' scripts directory..."
+  fi
   cp -a Git "$HOME/.local/share/nautilus/scripts"
 else
   echo "Error installing scripts to $HOME/.local/share/nautilus/scripts"
